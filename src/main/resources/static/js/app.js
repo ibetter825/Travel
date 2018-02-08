e0728414f3e878cdd088e2b4ca7b99856c3fe093
ibetter825 <y825y@qq.com>
2018-02-08 23:49:43 +0800

    rf

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -49,5 +49,65 @@ var app = {};
 	    script = document.createElement('script');
 	    script.setAttribute('src', url);
 	    document.body.appendChild(script);
+	},
+	/**
+	 * localStorage
+	 * key
+	 * value == null 删除
+	 */
+	app.local = function(key, value) {
+	    let storage = window.localStorage
+	        //如果浏览器不支持localStorage存到Cookie中
+	        //...
+	        //如果value未定义则为获取key对应的值
+	    if (value === undefined)
+	        return storage.getItem(key)
+	    else //反之则为设置或修改值或添加值或删除值
+	        value === null ? storage.removeItem(key) : storage.setItem(key, value)
 	};
+
+	/**
+	 * sessionStorage
+	 * key
+	 * value == null 删除
+	 */
+	app.session = function(key, value) {
+	    let storage = window.sessionStorage
+	        //如果浏览器不支持localStorage存到Cookie中
+	        //...
+	        //如果value未定义则为获取key对应的值
+	    if (value === undefined)
+	        return storage.getItem(key)
+	    else //反之则为设置或修改值或添加值或删除值
+	        value === null ? storage.removeItem(key) : storage.setItem(key, value)
+	};
+
+	/**
+	 * cookie
+	 * key
+	 * days 过期天数
+	 * value 值
+	 * opt 'del' 删除
+	 */
+	app.cookie = function(key, value, days) {
+	    if (value === undefined && document.cookie.length > 0) { //获取cookie
+	        let arr, reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)");
+	        if (arr = document.cookie.match(reg))
+	            return unescape(arr[2])
+	        else
+	            return null
+	    } else {
+	        let exdate = new Date();
+	        if (value === null) {
+	            exdate.setTime(exdate.getTime() - 1);
+	            let cval = cookie(key);
+	            if (cval != null)
+	                document.cookie = key + "=" + cval + ";expires=" + exdate.toGMTString();
+	        } else {
+	            exdate.setDate(exdate.getDate() + days ? days : 0)
+	            document.cookie = key + "=" + escape(value) +
+	                (days ? "" : ";expires=" + exdate.toGMTString())
+	        }
+	    }
+	}
 }(app));
