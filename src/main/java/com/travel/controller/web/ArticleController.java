b65b452e3b68b8dc0adce5d1ab2de557e0125195
ibetter825 <y825y@qq.com>
2018-02-09 23:57:05 +0800

    s

--- src/main/java/com/travel/controller/web/ArticleController.java
+++ src/main/java/com/travel/controller/web/ArticleController.java
@@ -1,6 +1,5 @@
 package com.travel.controller.web;
 
-import java.io.IOException;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +9,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.google.common.collect.Maps;
 import com.travel.annotation.Permission;
 import com.travel.bean.entity.Article;
-import com.travel.bean.entity.ArticleCount;
-import com.travel.bean.model.ResultModel;
-import com.travel.service.ArticleCountService;
 import com.travel.service.ArticleService;
 
 
@@ -25,8 +21,6 @@ import com.travel.service.ArticleService;
 public class ArticleController extends WebBaseController {
 	@Autowired
 	private ArticleService articleService;
-	@Autowired
-	private ArticleCountService articleCountService;
 	
 	@RequestMapping("/{id}.html")
 	public ModelAndView index(@PathVariable("id") Long id){
@@ -43,18 +37,5 @@ public class ArticleController extends WebBaseController {
 	@RequestMapping("/write.html")
 	public ModelAndView index(){
 		return new ModelAndView(getView("write"));
-	}
-	
-	/**
-	 * 查询文章的统计数据
-	 * @param id
-	 * @throws IOException 
-	 */
-	@RequestMapping("/{id}/count.json")
-	public void count(@PathVariable("id") Long id) throws IOException{
-		ResultModel model = new ResultModel();
-		ArticleCount count = articleCountService.query(id);
-		model.getData().put("count", count);
-		renderJson(model);
 	}
 }
